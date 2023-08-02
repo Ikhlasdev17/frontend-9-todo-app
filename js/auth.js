e84c551d83ed84b057331ca3518653333c9c39cd
Ikhlasdev17 <iklasaralbaev@gmail.com>
2023-08-02 17:50:26 +0500

    authorization completed successful

--- js/auth.js
+++ js/auth.js
@@ -24,3 +24,64 @@ changeToRegisterBtn.addEventListener('click', () => {
 	registerForm.classList.add('active')
 	authSubTitle.innerHTML = 'Register'
 })
+
+function register(e) {
+	e.preventDefault()
+
+	const data = {
+		phone: registerPhone.value,
+		name: registerName.value,
+		password: registerPassword.value,
+	}
+
+	fetch('http://todo.paydali.uz/api/register', {
+		method: 'POST',
+		headers: {
+			'Content-type': 'Application/json',
+		},
+		body: JSON.stringify(data),
+	})
+		.then(res => res.json())
+		.then(data => {
+			if (data.code === 200) {
+				Swal.fire('Good job!', 'You registered successful!', 'success')
+				registerForm.classList.remove('active')
+				loginForm.classList.add('active')
+				authSubTitle.innerHTML = 'Sign in'
+			} else {
+				Swal.fire('Wrong!', data.message, 'error')
+			}
+		})
+		.catch(err => console.log(err))
+}
+
+function login(e) {
+	e.preventDefault()
+
+	const data = {
+		phone: loginPhone.value,
+		password: loginPassword.value,
+	}
+
+	fetch('http://todo.paydali.uz/api/login', {
+		method: 'POST',
+		headers: {
+			'Content-type': 'Application/json',
+		},
+		body: JSON.stringify(data),
+	})
+		.then(res => res.json())
+		.then(data => {
+			if (data.code === 200) {
+				Swal.fire('Good job!', 'You signed in successful!', 'success')
+				localStorage.setItem('todo-token', data.payload.token)
+				localStorage.setItem('todo-username', data.payload.name)
+			} else {
+				Swal.fire('Wrong!', data.message, 'error')
+			}
+		})
+		.catch(err => console.log(err))
+}
+
+loginForm.addEventListener('submit', login)
+registerForm.addEventListener('submit', register)
